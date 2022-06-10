66ef2b1526ccc73ffee39ccc1246569770066bb4
AnonymousXC <cvxanonymous007@gmail.com>
2022-06-10 15:21:07 +0530

    Landing Page completed.

--- src/js/parallexBG.js
+++ src/js/parallexBG.js
@@ -11,10 +11,10 @@ window.addEventListener("scroll" , () => {
     let value = scrollY;
 
     stars.style.left = value * 0.25 + "px";
-    moon.style.top = value * 1.05 + "px";
+    moon.style.top = `clamp(0px, ${value * 1.05 + "px"}, 1000px)`;
     mountains_behind.style.top = value * 0.5 + "px";
     mountains_front.style.top = value * 0 + "px";
     typing.style.marginTop = value * 0.2 + "px";
     navBar.style.top = value * 0.5 + "px";
-    explore.style.marginTop = value * 1.4 + "px";
-})
+    explore.style.marginTop = `clamp(0px, ${value * 1.4 + "px"}, 1000px)`;
+});
